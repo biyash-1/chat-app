ee4b21661ba1fa41c1f0fbeb208538390935525b
biyash-1 <sthaalish39@gmail.com>
2024-11-30 00:02:29 +0545

    design

---
+++ app/signup/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      helllo this is signup component we are making it from scratch so lets get connected with each other
+    </div>
+  )
+}
+
+export default page
