34290be80a6778f288f69199a23ce22348b377c9
biyash-1 <sthaalish39@gmail.com>
2024-12-18 23:06:14 +0545

    "home page"

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,11 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
      
       <h1 className="text-4xl ">Im buidling a chat app</h1>
+      <div>
+        <p className="text-xl"> 
+          Be coonected with ur loved ones far from u within seconds.. 
+        </p>
+      </div>
       <Button onClick={handleLogin}>Login to Chat</Button>
     </div>
   );
