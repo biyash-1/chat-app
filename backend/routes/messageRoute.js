9ebd5285e7fc59f32ab4829b0b38042ddc2a86ef
biyash-1 <sthaalish39@gmail.com>
2024-12-21 22:03:12 +0545

    "endpoint"

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -2,13 +2,13 @@ import express from "express";
 const router = express.Router();
 import authenticate from "../middleware/authmiddleware.js"
 
-import {getMessages, getUsersForSidebar} from "../controllers/messageController.js";
+import {getMessages, getUsersForSidebar, sendMessage} from "../controllers/messageController.js";
 
 
 router.get("/getUsers", authenticate,getUsersForSidebar);
 
 router.get("/:id", authenticate, getMessages);
-
+router.post("/send/:id", authenticate, sendMessage);
 
 export default router;
 
