b8c3f0d163f57ac0da2bd01e6815d04a0eb06def
biyash-1 <sthaalish39@gmail.com>
2024-12-22 17:48:12 +0545

    "protected route enhanced"

--- lib/withAuth.tsx
+++ lib/withAuth.tsx
@@ -2,21 +2,25 @@ import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/app/store/useAuthStore";
 import { useEffect, FC } from "react";
 
-
 const withAuth = <P extends object>(WrappedComponent: FC<P>): FC<P> => {
   const AuthComponent: FC<P> = (props) => {
-    const { authUser,checkAuth } = useAuthStore();
+    const { authUser, hasHydrated } = useAuthStore();
     const router = useRouter();
 
     useEffect(() => {
-      if (!checkAuth) {
-console.log("WithAuth - Current state:", { authUser });
+      if (hasHydrated && authUser === null) {
         router.push("/login");
       }
-    }, [authUser, router]);
+    }, [authUser, hasHydrated]);
 
-    
-    
+    if (!hasHydrated) {
+      return null; // Wait until hydration completes
+    }
+
+    if (!authUser) {
+      return null; // Prevent rendering until auth is checked
+    }
+
     return <WrappedComponent {...props} />;
   };
 
