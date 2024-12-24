2cc8cb211bf60fc4dfcb30219c9308d5f260a11e
biyash-1 <sthaalish39@gmail.com>
2024-12-24 22:41:32 +0545

    "chamge"

--- app/chatpage/page.tsx
+++ app/chatpage/page.tsx
@@ -6,22 +6,23 @@ import { useChatStore } from "../store/useChatStore";
 import NoChatSelected from "@/components/NoChatSelected";
 import ChatContainer from "@/components/ChatContainer";
 import Sidebar from "@/components/Sidebar";
+
 const Page = () => {
-  const { authUser } = useAuthStore();
+  const { authUser,onlineUsers } = useAuthStore();
   console.log("auth status of chat page", authUser);
-  const {selectedUser} = useChatStore();
+  console.log("online users", onlineUsers);
+  const { selectedUser } = useChatStore();
 
   return (
     <div className="h-screen">
-      <div className="flex items-center justify-center pt-20 px-4 ">
-        <div className="bg-base-100 rounded-lg w-full max-w-6xl h-[calc(100vh-4rem)]">
-         <div className="flex h-full rounded-lg overflow-hidden">
-        <Sidebar/>
-        {selectedUser ? <ChatContainer/> : <NoChatSelected/>}
-         </div>
-      </div>
-         
+      <div className="flex items-center justify-center pt-10">
+        <div className="bg-slate-900 rounded-lg w-full max-w-6xl h-[calc(100vh-4rem)] px-4">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+            {selectedUser ? <ChatContainer /> : <NoChatSelected />}
+          </div>
         </div>
+      </div>
     </div>
   );
 };
