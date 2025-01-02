ca68ae6972a1808ff7e7957d28fbee54d2714ee1
biyash-1 <sthaalish39@gmail.com>
2025-01-02 23:25:57 +0545

    layout

---
+++ app/chatpage/_layout.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import WithAuth from "../../lib/withAuth";
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  const AuthenticatedComponent = WithAuth(() => <>{children}</>);
+
+  return <AuthenticatedComponent />;
+}
